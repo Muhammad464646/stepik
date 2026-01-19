d477a0d25ba0e27be8500628bbef7f236bfaa597
Mbuhoriev55@gmai.com <Mbuhoriev55@gmail.com>
2026-01-19 14:08:26 +0500

    done with views

--- project/views.py
+++ project/views.py
@@ -1,3 +1,38 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets, permissions
+from .models import Course, Module, Task, Submission, InputOutput, Enrollment
+from .serializers import CourseSerializer, ModuleSerializer, TaskSerializer, SubmissionSerializer, InputOutputSerializer, EnrollmentSerializer
 # Create your views here.
+
+class CourseViewSet(viewsets.ModelViewSet):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+class ModuleViewSet(viewsets.ModelViewSet):
+    queryset = Module.objects.all()
+    serializer_class = ModuleSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+class SubmissionViewSet(viewsets.ModelViewSet):
+    queryset = Submission.objects.all()
+    serializer_class = SubmissionSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+class InputOutputViewSet(viewsets.ModelViewSet):
+    queryset = InputOutput.objects.all()
+    serializer_class = InputOutputSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+class EnrollmentViewSet(viewsets.ModelViewSet):
+    queryset = Enrollment.objects.all()
+    serializer_class = EnrollmentSerializer
+    permission_classes = [permissions.IsAuthenticated]
